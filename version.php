b4aef6dc7a5b5a9e5f007730a90891f80a363d47
Adam Olley <adam.olley@netspot.com.au>
2013-04-30 10:23:26 +0930

    Increase dependecy version number to match updated report

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die;
 $plugin->version   = 2013043000;       // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2012112900;       // Requires this Moodle version
 $plugin->component = 'block_engagement'; // Full name of the plugin (used for diagnostics).
-$plugin->dependencies = array('mod_engagement' => 2012031400, 'report_engagement' => 2012031300);
+$plugin->dependencies = array('mod_engagement' => 2012031400, 'report_engagement' => 2013043000);
 
 $plugin->maturity = MATURITY_STABLE;
