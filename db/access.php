004c9bac7ed3a1aa0c30b00336c9df3ebc58552d
Adam Olley <adam.olley@netspot.com.au>
2013-09-03 12:51:25 +0930

    Docblock in access.php

--- db/access.php
+++ db/access.php
@@ -14,11 +14,22 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * Engagement block caps.
+ *
+ * @package    block_engagement
+ * @copyright  NetSpot Pty Ltd
+ * @author     Adam Olley <adam.olley@netspot.com.au>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 defined('MOODLE_INTERNAL') || die();
 
 $capabilities = array(
 
     'block/engagement:addinstance' => array(
+        'riskbitmask' => RISK_SPAM | RISK_XSS,
+
         'captype' => 'write',
         'contextlevel' => CONTEXT_BLOCK,
         'archetypes' => array(
